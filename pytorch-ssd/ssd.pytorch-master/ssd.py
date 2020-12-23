b4cb423a4dc73d233146bcc31d243ef0ec1dba2e
TJUdyk <1204712609@qq.com>
2020-12-23 16:59:47 +0800

    添加了新的注释

--- pytorch-ssd/ssd.pytorch-master/ssd.py
+++ pytorch-ssd/ssd.pytorch-master/ssd.py
@@ -8,15 +8,10 @@ import os
 
 
 class SSD(nn.Module):
-    """Single Shot Multibox Architecture
-    The network is composed of a base VGG network followed by the
-    added multibox conv layers.
-    Each multibox layer branches into
-        1) conv2d for class conf scores
-        2) conv2d for localization predictions
-        3) associated priorbox layer to produce default bounding boxes specific to the layer's feature map size.
-    # 初始化ssd网络
-    """
+    # SSD网络是由 VGG 网络后几个 multibox 卷积层 组成的, 每一个 multibox 层会有如下分支:
+    # - 用于class conf scores的卷积层
+    # - 用于localization predictions的卷积层
+    # - 与priorbox layer相关联, 产生默认的bounding box
     def __init__(self, phase, size, base, extras, head, num_classes):
         """
         :param phase: (string) Can be "test" or "train"
@@ -24,12 +19,14 @@ class SSD(nn.Module):
         :param base: VGG16 layers for input, size of either 300 or 500，VGG16的层
         :param extras: 将输出结果送到multibox loc和conf layers的额外的层
         :param head:"multibox head", 包含一系列的loc和conf卷积层.
+        head=(loc_layers, conf_layers)
         :param num_classes:
         """
         super(SSD, self).__init__()
         self.phase = phase
-        # num_classes=[21,-2]
+        # num_classes=[21,-2]，还是不是很理解21和-2
         self.num_classes = num_classes
+        #cfg是什么？？跟coco和voc的数据集有关
         self.cfg = (coco, voc)[num_classes == 21]
         # layers/functions/prior_box.py class PriorBox(object)
         self.priorbox = PriorBox(self.cfg)
@@ -37,14 +34,13 @@ class SSD(nn.Module):
         self.priors = Variable(self.priorbox.forward(), volatile=True)
         self.size = size
 
-        # SSD network
+        # 使用modluelist可以自动添加到整个网络上
+        # SSD network base是vgg的网络
         self.vgg = nn.ModuleList(base)
         # Layer learns to scale the l2 normalized features from conv4_3
-        # 为什么这里要用到L2Normal
         self.L2Norm = L2Norm(512, 20)
-        # 使用modluelist可以自动添加到整个网络上
         self.extras = nn.ModuleList(extras)
-        # head = (loc_layers, conf_layers)
+        # head = (loc_layers, conf_layers)，从multibox中返回的参数
         self.loc = nn.ModuleList(head[0])
         self.conf = nn.ModuleList(head[1])
 
@@ -65,7 +61,8 @@ class SSD(nn.Module):
         #       1: confidence layers, Shape: [batch*num_priors, num_classes]
         #       2: localization layers, Shape: [batch, num_priors*4]
         #       3: priorbox layers, Shape: [2, num_priors*4]
-        # 这个列表存储的是参与预测的卷积层的输出, 也就是原文中那6个指定的卷积层
+
+        # 这个列表存 储的是参与预测的卷积层的输出, 也就是原文中那6个指定的卷积层
         sources = list()
         # 用于存储预测的边框信息
         loc = list()
@@ -75,36 +72,63 @@ class SSD(nn.Module):
         # apply vgg up to conv4_3 relu
         for k in range(23):
             x = self.vgg[k](x)
-
         s = self.L2Norm(x)
+        # 将 conv4_3 的特征层输出添加到 sources 中, 后面会根据 sources 中的元素进行预测
         sources.append(s)
 
-        # apply vgg up to fc7
+        # apply vgg up to fc7 # 将vgg应用到fc7
         for k in range(23, len(self.vgg)):
             x = self.vgg[k](x)
         sources.append(x)
 
         # apply extra layers and cache source layer outputs
+        # 计算extras layers, 并且将结果存储到sources列表中
         for k, v in enumerate(self.extras):
+            # 为什么这里的relu函数这样写？？？？为什么不用nn.ReLU
             x = F.relu(v(x), inplace=True)
+            # 只添加奇数层
             if k % 2 == 1:
+                # 同理, 添加到 sources 列表中
                 sources.append(x)
 
-        # apply multibox head to source layers
+        # 应用multibox到source layers上, source layers中的元素均为各个用于预测的特征图谱
+        # apply multibox to source layers
+
+        # 注意pytorch中卷积层的输入输出维度是:[N×C×H×W]
         for (x, l, c) in zip(sources, self.loc, self.conf):
+            # loc: [b×w1×h1×4*4, b×w2×h2×6*4, b×w3×h3×6*4, b×w4×h4×6*4, b×w5×h5×4*4, b×w6×h6×4*4]
+            # conf: [b×w1×h1×4*C, b×w2×h2×6*C, b×w3×h3×6*C, b×w4×h4×6*C, b×w5×h5×4*C, b×w6×h6×4*C] C为num_classes
+            # 数据做完行和列的转换的话，就变得不连续了！！！，所以要用contiguous进行转换
             loc.append(l(x).permute(0, 2, 3, 1).contiguous())
             conf.append(c(x).permute(0, 2, 3, 1).contiguous())
 
+            # cat 是 concatenate 的缩写, view返回一个新的tensor, 具有相同的数据但是不同的size, 类似于numpy的reshape
+            # 在调用view之前, 需要先调用contiguous？？？？why？？ 原来的tensor是不连续的，调用contiguous可以让tensor变得连续
+            # permute重新排列维度顺序, PyTorch维度的默认排列顺序为 (N, C, H, W),
+            # 因此, 这里的排列是将其改为 $(N, H, W, C)$.
+            # contiguous返回内存连续的tensor, 由于在执行permute或者transpose等操作之后, tensor的内存地址可能不是连续的,
+            # 然后 view 操作是基于连续地址的, 因此, 需要调用contiguous语句.
+            #这里的o是什么意思？？？？
         loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
         conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+        # 将除batch以外的其他维度合并, 因此, 对于边框坐标来说, 最终的shape为(两维):[batch, num_boxes*4]
+        # 同理, 最终的shape为(两维):[batch, num_boxes*num_classes]
+
         if self.phase == "test":
+            # 这里用到了 detect 对象, 该对象主要由于接预测出来的结果进行解析, 以获得方便可视化的边框坐标和类别编号, 具体实现会在后文讨论.
             output = self.detect(
+            output = self.detect(
+                #  又将shape转换成: [batch, num_boxes, 4], 即[1, 8732, 4]
                 loc.view(loc.size(0), -1, 4),                   # loc preds
+                # 同理,  shape 为[batch, num_boxes, num_classes], 即 [1, 8732, 21]
                 self.softmax(conf.view(conf.size(0), -1,
                              self.num_classes)),                # conf preds
                 self.priors.type(type(x.data))                  # default boxes
+                # 利用 PriorBox对象获取特征图谱上的 default box, 该参数的shape为: [8732,4]. 关于生成 default box 的方法实际上很简单, 类似于 anchor box, 详细的代码实现会在后文解析.
+                # 这里的 self.priors.type(type(x.data)) 与 self.priors 就结果而言完全等价(自己试验过了), 但是为什么?
             )
         else:
+            # 如果是训练阶段, 则无需解析预测结果, 直接返回然后求损失.
             output = (
                 loc.view(loc.size(0), -1, 4),
                 conf.view(conf.size(0), -1, self.num_classes),
